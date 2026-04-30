3837d6edc56861453913593c7e8cebc6f0d6b986
Pedro Santiago <santiagopedrodev@gmail.com>
2026-04-30 14:23:02 -0300

    oops

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ OLLAMA_BASE_URL = "http://10.10.10.152:11434"
 COORDINATOR_MODEL = "qwen3.5:9b"
 WORKER_MODEL = "qwen3.5:2b"
 
-COORDINATOR_MODEL = "phi3.5:latest"
-WORKER_MODEL = "phi3.5:latest"
+# COORDINATOR_MODEL = "phi3.5:latest"
+# WORKER_MODEL = "phi3.5:latest"
